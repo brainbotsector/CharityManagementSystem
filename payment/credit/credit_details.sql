2ce532f99913a832d92bf8a02cbb3c1e4ed589be
brainbotsector <88769793+brainbotsector@users.noreply.github.com>
2021-08-11 16:03:54 +0530

    Add files via upload

---
+++ payment/credit/credit_details.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Oct 27, 2020 at 05:50 AM
+-- Server version: 5.7.26
+-- PHP Version: 7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `credit_details`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `credit_card`
+--
+
+DROP TABLE IF EXISTS `credit_card`;
+CREATE TABLE IF NOT EXISTS `credit_card` (
+  `cardno` varchar(100) NOT NULL,
+  `expmonth` varchar(100) NOT NULL,
+  `expyear` varchar(100) NOT NULL,
+  `cvv` varchar(30) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `amount` varchar(100) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ payment/credit/include/datab.php
@@ -0,0 +1,5 @@
+<?php
+
+$connect = new PDO("mysql:host=localhost;dbname=credit_details", "root", "");
+
+?>
